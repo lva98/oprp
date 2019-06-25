c22b92e0889de70cc8580a02c39b6f28163344fb
Leonardo Valério <leonardovalerio@live.com>
2019-06-25 17:17:17 -0300

    Add files via upload

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ echo "---Compilando"
 mpic++ main.cpp -fopenmp -lcrypt
 
 echo "---Copiando arquivos"
-scp * client:~/Code/OPRP/
+#scp * client:~/Code/OPRP/
 
 echo "---Execuçao"
 mpirun -machinefile machines a.out
--- source.hpp
+++ source.hpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
+#include <crypt.h>
 using namespace std;
 
 namespace oprp {    
@@ -13,22 +14,21 @@ namespace oprp {
     map< string, set< string > > passwords_buff;
     map< string, bool> broken;
 
-
-    string alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ./";
+    string alpha = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz./";
 
     namespace {
         vector< string > roots;
 
-        void is_valid(string &s) {
+        void is_valid(string &s, struct crypt_data & c_data) {
             char * result;
             for(auto & vec : passwords_buff) {
-                result = crypt(s.data(), vec.first.data());
+                result = crypt_r(s.c_str(), vec.first.c_str(), &c_data);
 
                 int i = 0;
                 for(auto & str : vec.second) {
                     if(!broken[str]) {
-                        if(strcmp(result, str.data()) == 0) {
-                            cout << str << " = " << s << endl << vec.first.data();
+                        if(strcmp(result, str.c_str()) == 0) {
+                            cout << str << " = " << s << endl;
                             broken[str] = true;
                         }
                     }
@@ -36,32 +36,32 @@ namespace oprp {
             }
         }
 
-        void worker_recursive(string s, int h, int max) {
+        void worker_recursive(string s, int h, int max, struct crypt_data & c_data) {
             if (h < max) {
-                for (int i = 0; i < 65; i++) {
+                for (int i = 0; i < 64; i++) {
                     string stemp = s + alpha[i];
                     //if(rank == size - 1)
-                    //    cout << stemp << endl;
-                    is_valid(s);
-                    worker_recursive(stemp, h + 1, max);
+                    cout << stemp << endl;
+                    is_valid(s, c_data);
+                    worker_recursive(stemp, h + 1, max, c_data);
                 }
             }
         }
 
-        void init_root(string s, int h) {
+        void init_root(string s, int h, struct crypt_data & c_data) {
             if(h < 2) {
                 for(int i = 0; i < 65; i++) {
                     string stemp = s + alpha[i];
 
                     //cout << stemp << endl;
                     if(rank == 0)
-                        is_valid(stemp);
+                        is_valid(stemp, c_data);
 
                     if(stemp.size() == 2) {
                         roots.push_back(stemp);
                     }
 
-                    init_root(stemp, h+1);
+                    init_root(stemp, h+1, c_data);
                 }
             }
         }
@@ -87,8 +87,11 @@ namespace oprp {
     }
 
     void start_up(string file) {
+        //c_data.initialized = 0;
         read_entry(file);
-        init_root("", 0);
+        struct crypt_data c_data;
+        c_data.initialized = 0;
+        init_root("", 0, c_data);
     }
 
     void run_recursive() {
@@ -105,11 +108,12 @@ namespace oprp {
         }
 
         //cout << "rank: " << rank << ", init: " << init << ", end: " << end << endl;
-
-#pragma omp parallel for num_threads(2)
+        struct crypt_data c_data;
+        c_data.initialized = 0;
+#pragma omp parallel for num_threads(2) private(c_data)
         for(int i = init; i <= end; i++) {
             for(int l = 3; l <= 8; l++ ) {
-                worker_recursive(roots[i], 2, l);
+                worker_recursive(roots[i], 2, l, c_data);
             }
         }
     }
